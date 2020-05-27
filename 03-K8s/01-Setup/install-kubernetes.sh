9220db99bc926d47f8ddd5255ba175e28921741d
root <root@master.example.com>
2020-05-27 05:33:23 +0000

    K8s-Install-Update

--- 03-K8s/01-Setup/install-kubernetes.sh
+++ 03-K8s/01-Setup/install-kubernetes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "installing docker"
+echo "Installing Docker"
 apt-get update
 apt-get install -y \
     apt-transport-https \
@@ -14,7 +14,7 @@ add-apt-repository \
    stable"
 apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
 
-echo "installing kubernetes"
+echo "Installing Kubernetes"
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -23,8 +23,8 @@ EOF
 apt-get update
 apt-get install -y kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
 
-echo "deploying kubernetes (with calico)..."
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="10.128.0.6" 
+echo "Deploying Kubernetes (with calico)..."
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.0.100" 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 # DigitalOcean without firewall (IP-in-IP allowed) - or any other cloud / on-prem that supports IP-in-IP traffic
